f08aa32589e73f9c9a04b1f56348b25963f14899
Sandeep Bhardwaj <sandeepbhardwaj01@gmail.com>
2020-02-16 18:08:48 +0530

    changed in json response as per ui

--- src/main/java/com/koko/payload/response/JwtResponse.java
+++ src/main/java/com/koko/payload/response/JwtResponse.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Getter
 @AllArgsConstructor
 public class JwtResponse {
-	private String token;
+	private String accessToken;
 	private String type = "Bearer";
 	private Long id;
 	private String username;
@@ -18,7 +18,7 @@ public class JwtResponse {
 	private List<String> roles;
 
 	public JwtResponse(String accessToken, Long id, String username, String email, List<String> roles) {
-		this.token = accessToken;
+		this.accessToken = accessToken;
 		this.id = id;
 		this.username = username;
 		this.email = email;
